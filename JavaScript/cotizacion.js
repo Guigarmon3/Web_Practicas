beecf8b8550e5aab7e75d2ab417ba4475cf6ddb5
Guigarmon3 <guigarmon3@alu.edu.gva.es>
2026-05-28 18:15:48 +0200

    css commit

--- JavaScript/cotizacion.js
+++ JavaScript/cotizacion.js
@@ -147,6 +147,7 @@ async function renderizarCotizaciones(lista) {
             const totalanio = await obtenerCotizacionesAgrupadasPorAnyo(anyo);
             const h3total = document.createElement('h3');
             h3total.textContent = `Total TGSS: ${totalanio.toFixed(2)} €`;
+            h3total.id = "totalTGB"
             divAnio.appendChild(h3total);
         } catch (error) {
             console.error(`Error al obtener cotizaciones para el año ${anyo}:`, error);
